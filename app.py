d106d1bdd4f73c3c7d8e5442a5f91f210818b766
spartanryorix <ryorix5899@gmail.com>
2024-06-09 16:07:39 +0300

    message

--- app.py
+++ app.py
@@ -1,69 +1,39 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request
 import mysql.connector
-import sys
 
 app=Flask(__name__)
-app.secret_key = 'rakybakyshine9595'
 
-
-#mydb= mysql.connector.connect(
-    #host='localhost',
-    #user='root',
-    #password='',
-    #database='library'
-#)
 
 @app.route("/", methods=['GET', 'POST'])
-def data():
-    mydb= mysql.connector.connect(
+def library():
+    connection= mysql.connector.connect(
         host='localhost',
         user='root',
         password='',
         database='library'
     )
+
+    cursor=connection.cursor()
+
     books_data=''
-    authors_data=''
+
     if request.method == 'POST':
         bookdata=request.form.get('bookname')
         descdata=request.form.get('description')
         datedata=request.form.get('releasedate')
-
         authordata=request.form.get('authorname')
-        query2='INSERT INTO authors (Name) VALUES (%s)'
-        val2=(authordata,)
-        mycursor = mydb.cursor()
-        mycursor.execute(query2, val2)
-        lastAuthorId=mycursor.lastrowid
-        authordata=mycursor.fetchall()
-        mydb.commit()
 
-        query='INSERT INTO books (Name, Description, Release_Date, Author_ID) VALUES (%s, %s, %s, %s)'
-        val=(bookdata, descdata, datedata, lastAuthorId)
-        mycursor = mydb.cursor()
-        mycursor.execute(query, val)
-        #query5="SELECT * FROM authors WHERE ID='ID'"
-        #mycursor.execute(query5)
-        #print(authordata)
+        cursor.execute('INSERT INTO authors (Name) VALUES (%s)', (authordata,))
+        lastAuthorId=cursor.lastrowid
 
+        cursor.execute('INSERT INTO books (Name, Description, Release_Date, Author_ID) VALUES (%s, %s, %s, %s)', (bookdata, descdata, datedata, lastAuthorId))
+        connection.commit()
 
-        mycursor = mydb.cursor(dictionary=True)
-        query3='SELECT books.ID, books.Name, books.Description, books.Release_Date, books.created_at, books.updated_at, books.Author_ID, authors.Name AS author_name FROM books LEFT JOIN authors ON books.Author_ID=authors.ID'
-        mycursor.execute(query3)
-        books_data = mycursor.fetchall()
-        print(books_data)
-        print("/////////")
+        cursor.execute("SELECT books.ID, books.Name, books.Description, books.Release_Date, books.created_at, books.updated_at, books.Author_ID, authors.Name AS author_name FROM books LEFT JOIN authors ON books.Author_ID=authors.ID;")
+        books_data = cursor.fetchall()
 
+        cursor.close()
+        connection.close()
+    return render_template('index.html', books_data=books_data)
 
-        query4='SELECT * FROM authors'
-        mycursor.execute(query4)
-        authors_data = mycursor.fetchall()
-        print(authors_data)
-        
-
-        mycursor.close()
-        mydb.close()
-        flash('Data added successfully!', 'success')
-    return render_template('index.html', books_data=books_data, authors_data=authors_data)
-
-if __name__ == "__main__":
-    app.run()
+app.run()
